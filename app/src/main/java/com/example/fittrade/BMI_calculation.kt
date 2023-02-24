4b04347563c9664219885ffc865c31eba4c43d4b
saminNazmus201 <sadatnazmus567@gmail.com>
2023-02-25 01:15:00 +0600

    bmi wrong-- samin

--- app/src/main/java/com/example/fittrade/BMI_calculation.kt
+++ app/src/main/java/com/example/fittrade/BMI_calculation.kt
@@ -15,16 +15,19 @@ class BMI_calculation : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            val height = binding.height.toString()
-            val dobheight= height.toDouble()
-            val weight= binding.weight.toString()
-            val dobweight = weight.toDouble()
-            val bmi= dobweight/(dobheight * dobheight)
-
-            binding.res.text = String.format("your bmi is : %.2f", bmi)
+            calculateBMI()
         }
 
     }
+    private fun calculateBMI(){
+        val height = binding.height.toString()
+        val h= height.toFloat()
 
+        val weight= binding.weight.toString()
+        val w = weight.toFloat()
+        val bmi= w/(h * h)
+        val dispaly = bmi.toString()
+        binding.res.text = dispaly
+    }
 
 }
