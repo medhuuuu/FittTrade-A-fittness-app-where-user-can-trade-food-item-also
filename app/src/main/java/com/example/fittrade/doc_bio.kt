f57d85988f81adecffa267632817d299326714da
medhuuuu <tamedha@gmail.com>
2023-03-26 22:53:46 +0600

    seller doctor sing up-- medha

--- app/src/main/java/com/example/fittrade/doc_bio.kt
+++ app/src/main/java/com/example/fittrade/doc_bio.kt
@@ -2,10 +2,80 @@ package com.example.fittrade
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class doc_bio : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var db = Firebase.firestore
+
+    private lateinit var tvJobName : TextView
+    private lateinit var etBio : EditText
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doc_bio)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        tvJobName = findViewById(R.id.tvjobName)
+        etBio = findViewById(R.id.textInputEditText)
+        button = findViewById(R.id.saveButton)
+
+        setData()
+
+        button.setOnClickListener {
+            val bioText = etBio.text.toString().trim()
+            val docUid = FirebaseAuth.getInstance().currentUser!!.uid
+
+            val ref = db.collection("doctor").document(docUid)
+
+            ref.get().addOnSuccessListener {
+                if (it != null) {
+                    val job = it.data?.get("job")?.toString()
+                    tvJobName.text = job
+                }
+            }
+
+            db.collection("gym-trainer").document(docUid).get().addOnSuccessListener {
+                if (it != null) {
+                    val job = it.data?.get("job")?.toString()
+                    tvJobName.text = job
+                }
+            }
+
+            val updateMap = mapOf("bio" to bioText)
+
+            ref.update(updateMap).addOnSuccessListener {
+                Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
+    }
+
+    private fun setData(){
+        val docUid = FirebaseAuth.getInstance().currentUser!!.uid
+
+        db.collection("doctor").document(docUid).get().addOnSuccessListener {
+
+            if (it != null) {
+                val bio =it.data?.get("bio")?.toString()
+
+                etBio.setText(bio)
+            }
+            Toast.makeText(this, "Added Done", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/fittrade/sellersignup.kt
+++ app/src/main/java/com/example/fittrade/sellersignup.kt
@@ -1,5 +1,6 @@
 package com.example.fittrade
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -27,8 +28,9 @@ class sellersignup : AppCompatActivity() {
     private lateinit var etPlace : EditText
     private lateinit var etPass : EditText
     private lateinit var etConpass : EditText
+    private lateinit var etSpinner: Spinner
+    private lateinit var etJob : TextView
     private lateinit var button: Button
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +38,14 @@ class sellersignup : AppCompatActivity() {
         binding = ActivitySellersignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val profession_names = arrayOf("Doctor","Seller","Gym-trainer")
-        val arrayadapter= ArrayAdapter(this,android.R.layout.simple_spinner_item,profession_names)
-        binding.spinbar.adapter = arrayadapter
+        etSpinner = findViewById(R.id.spinbar)
+        etJob = findViewById(R.id.spinshow)
 
-        binding.spinbar.onItemSelectedListener = object :
+        val profession_names = arrayOf("Doctor","Seller","Gym-trainer")
+        val arrayAdapter= ArrayAdapter(this,android.R.layout.simple_spinner_item,profession_names)
+        etSpinner.adapter = arrayAdapter
+
+        etSpinner.onItemSelectedListener = object :
 
         AdapterView.OnItemSelectedListener{
             override fun onItemSelected(
@@ -49,7 +54,8 @@ class sellersignup : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                binding.spinshow.text= profession_names[position]
+                parent?.getItemAtPosition(position).toString()
+                etJob.text= profession_names[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -77,6 +83,8 @@ class sellersignup : AppCompatActivity() {
             val place = etPlace.text.toString().trim()
             val pass = etPass.text.toString().trim()
             val conPass = etConpass.text.toString().trim()
+            val jobText = etJob.text.toString().trim()
+            val bio = " "
 
             if(companyName.isNotEmpty() && phone.isNotEmpty() && place.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && conPass.isNotEmpty()){
                 if (pass == conPass) {
@@ -87,15 +95,34 @@ class sellersignup : AppCompatActivity() {
                                 "email" to email,
                                 "phone" to phone,
                                 "pass" to pass,
-                                "conPass" to conPass
+                                "job" to jobText,
+                                "bio" to bio
                             )
 
                             val sellerId = FirebaseAuth.getInstance().currentUser!!.uid
 
-                            db.collection("seller").document(sellerId).set(sellerMap).addOnSuccessListener {
-                                Toast.makeText(this, "Added Done", Toast.LENGTH_SHORT).show()
-                            }.addOnFailureListener {
-                                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                            if (jobText == "Doctor"){
+                                db.collection("doctor").document(sellerId).set(sellerMap).addOnSuccessListener {
+                                    Toast.makeText(this, "Added Done", Toast.LENGTH_SHORT).show()
+                                    startActivity(Intent(this, doc_bio::class.java))
+                                }.addOnFailureListener {
+                                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            else if (jobText == "Seller"){
+                                db.collection("seller").document(sellerId).set(sellerMap).addOnSuccessListener {
+                                    Toast.makeText(this, "Added Done", Toast.LENGTH_SHORT).show()
+                                }.addOnFailureListener {
+                                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            else if (jobText == "Gym-trainer"){
+                                db.collection("gym-trainer").document(sellerId).set(sellerMap).addOnSuccessListener {
+                                    Toast.makeText(this, "Added Done", Toast.LENGTH_SHORT).show()
+                                    startActivity(Intent(this, doc_bio::class.java))
+                                }.addOnFailureListener {
+                                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                                }
                             }
                         }
                         else{
